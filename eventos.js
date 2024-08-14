8397fdc8bddc448fd20ced665dbcf8d61f2373e4
lvbd99 <lvbd99@gmail.com>
2024-08-14 13:34:22 -0300

    agrego js

---
+++ eventos.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+let div = document.getElementById("div");
+
+div.addEventListener("click", function() {
+    alert("Hola, soy el div!");
+});
+
+})
